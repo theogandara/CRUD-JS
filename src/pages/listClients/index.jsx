66a1089d7a0743a7d93a74980681656a818d86fb
Théo Gandara <theogandara@gmail.com>
2022-02-27 01:15:51 -0300

    added libs - hook-form and sty-comp

---
+++ src/pages/listClients/index.jsx
@@ -0,0 +1,9 @@
+const ClientsPage = () => {
+  return (
+    <div>
+      <h1>Cadastrar cliente;</h1>
+    </div>
+  );
+};
+
+export default ClientsPage;
---
+++ src/pages/register/index.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div>
+      <h1>Cadastrar cliente;</h1>
+    </div>
+  );
+};
+
+export default Register;
