7dbdb58c1aeb3f3295c941e44acc1c317dabcd3c
Théo Gandara <theogandara@gmail.com>
2022-02-28 14:31:55 -0300

    feat: input width

--- src/components/Input/styled.js
+++ src/components/Input/styled.js
@@ -1,24 +1,25 @@
 import styled from "styled-components";
 
 export const BackgroundInput = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Label = styled.label`
-    font-size: 18px;
-    margin: 5px 0px 0px 0px;
-`
+  font-size: 18px;
+  margin: 5px 0px 0px 0px;
+`;
 
 export const Error = styled.p`
-    height: 20px;
-    color: red;
-    font-size: 13px;
-    margin: 0;
-    margin-bottom: 5px;
-`
+  height: 20px;
+  color: red;
+  font-size: 13px;
+  margin: 0;
+  margin-bottom: 5px;
+`;
 
 export const InputStyle = styled.input`
-    margin-top: 5px;
-    height: 30px;
-`
+  width: 230px;
+  margin-top: 5px;
+  height: 30px;
+`;
